0cda791f10133337ad85fcf8303363aac4eb2bd9
Macflaims <martinsanchez2533@gmail.com>
2023-11-13 14:09:21 -0300

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: "/DobleLL/",
-  plugins: [react()]
+  esbuild: {
+    jsxInject: `import React from 'react'`
+  },
+  plugins: [react()]
 })
